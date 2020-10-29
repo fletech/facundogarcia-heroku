e51dd1c3b28e1cf24f3f7fe7537aa2723dc4df1a
fletech <facundogarcia.ar@gmail.com>
2020-10-29 18:57:46 +0100

    Primer commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, './public')));
+
+app.get('/', function (req, res) {
+     res.sendFile(path.join(__dirname, '/views/home.html'))
+   })
+   
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log('Server running...')
+})
+
